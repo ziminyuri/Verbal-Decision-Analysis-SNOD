0b3286c3f5b636bcdd140d6001a5a79b371778fb
ziminyuri <zimin.yuri22@gmail.com>
2020-11-15 11:33:57 +0300

    before fix select

---
+++ frontend/src/context/NewModelContext.js
@@ -0,0 +1,6 @@
+import {createContext} from 'react'
+
+export const NewModelContext = createContext ({
+    numberAlternative: null,
+    nummberСriteria: null,
+})
--- frontend/src/pages/CreateModelPage.js
+++ frontend/src/pages/CreateModelPage.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useState} from "react";
 
 
-export const CreateModelPage = (props) => {
-    const optionValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+export const CreateModelPage = () => {
+    const optionValue = [2, 3, 4, 5, 6, 7, 8, 9, 10]
     useEffect(() => {
         var elems1 = document.getElementById("sel1");
         var elems2 = document.getElementById("sel2");
@@ -10,50 +10,72 @@ export const CreateModelPage = (props) => {
         var instances2 = window.M.FormSelect.init(elems2, {});
     }, []);
 
+    useEffect(() =>{
+        window.M.updateTextFields()
+    }, [])
 
-    return (
-        <div className="row">
-            <div className="col s6 offset-s3">
-                <div className="card blue-grey lighten-5">
-                    <div className="card-content">
-                        <div className="input-field">
-                            <select id="sel1">
-                                <option value="" disabled selected>
-                                    Выберите кол-во
-                                </option>
+    const [alternative, setAlternative] = useState(1)
+    const [criteria, setCriteria] = useState(1)
+    const [flagInputData, setFlagInputData] = useState (false)
 
-                                {optionValue.map(v => (
-                                    <option value={v}>Альтернатив: {v}</option>
-                                ))}
 
-                            </select>
-                            <label>Кол-во альтернатив</label>
+    const inputDataHandler = () => {
+        console.log('сравпып')
+        console.log(criteria)
+        setFlagInputData(true)
+
+    }
+    function ShowingContent(){
+        if (flagInputData) {
+            return (<p> Таблица</p>)
+        }
+        else{
+            return (
+                <div className="row">
+                <div className="col s6 offset-s3">
+                    <div className="card blue-grey lighten-5">
+                        <div className="card-content">
+                            <div className="input-field">
+                                <select id="sel1" value={alternative} name="alternative" onChange={e => setAlternative(e.target.value)}>
+                                    {optionValue.map(v => (
+                                        <option key={v} value={v} >Альтернатив: {v}</option>
+                                    ))}
+
+                                </select>
+
+                                <label>Кол-во альтернатив</label>
+                            </div>
+
+                            <div className="input-field">
+                                <select id="sel2" value={criteria} name="criteria" onChange={e => setCriteria(e.target.value)}>
+
+
+
+                                    {optionValue.map(v => (
+                                        <option key={v} value={v}>Критериев: {v}</option>
+                                    ))}
+
+                                </select>
+                                <label>Кол-во критериев</label>
+                            </div>
                         </div>
+                        <div className="card-action">
+                            <button
+                                className='btn auth-btn-custom'
+                                onClick={inputDataHandler}
 
-                        <div className="input-field">
-                            <select id="sel2">
-                                <option value="" disabled selected>
-                                    Выберите кол-во
-                                </option>
-
-                                {optionValue.map(v => (
-                                    <option value={v}>Критериев: {v}</option>
-                                ))}
-
-                            </select>
-                            <label>Кол-во критериев</label>
+                            >Далее</button>
+                            <button className='btn auth-btn-custom'>Назад</button>
                         </div>
-                    </div>
-                    <div className="card-action">
-                        <button
-                            className='btn auth-btn-custom'
-
-                        >Далее</button>
-                        <button className='btn auth-btn-custom'>Назад</button>
                     </div>
                 </div>
             </div>
-        </div>
+            )
+        }
+    }
+
+    return (
+        <ShowingContent/>
     );
 };
 
